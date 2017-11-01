c2e047a4a985a633d3055f2847c7e107dd3f07ac
Will Chen <32256929+jacchwill@users.noreply.github.com>
2017-11-01 22:13:16 +0800

    file write close

---
+++ 1101.py
@@ -0,0 +1,25 @@
+print "陳威呈"
+for f in range(0,10):
+    print f
+    if f==7:
+        print "lucky"
+
+s=open("e.txt","w")
+s.write("陳威呈e")
+s.close()
+
+s=open("1.txt","w")
+s.write("1")
+s.close()
+
+s=open("2.txt","w")
+s.write("2")
+s.close()
+
+s=open("3.txt","w")
+s.write("3")
+s.close()
+
+s=open("4.txt","w")
+s.write("4")
+s.close()
